919caecde66979d74759b8b832ced21e088ea92d
Pandey <prerit.pandey@accenture.com>
2021-02-17 18:51:20 +0530

    'update'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
+import react, { useEffect, useState } from 'react';
 import './App.css';
 
+import { API } from 'aws-amplify'
+import { withAuthenticator } from 'aws-amplify-react'
+
 function App() {
+  const [people,updatePeople] = useState ([])
+  async function callApi() {
+    try {
+     const peopleData= await API.get('mainappapi','/people')
+     updatePeople(peopleData,people)
+     console.log('peopleData : ',peopleData)
+    
+      const coinData=await API.get('mainappapi','/coins')
+      console.log('coinData : ',coinData)
+    }catch(err){
+      console.log({ err })
+
+    }
+  }
+  useEffect(()=>{
+    callApi()
+  },[])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Hello World !</h1>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
